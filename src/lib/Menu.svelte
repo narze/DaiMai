809078b40b96eddfda8138422a31cb5db121d949
santhitak <santhita.sk@gmail.com>
2021-10-23 00:06:10 +0700

    Add: github link animation

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -7,8 +7,32 @@
   export let items: Item[] = []
 </script>
 
-<div class="text-xs fixed sm:text-base bottom-4 right-4 text-right z-10">
-  {#each items as { name, url }}
-    <a href={url} class="p-1 bg-white rounded" target="_blank" rel="noreferrer">{name}</a>
-  {/each}
-</div>
+<style>
+  button:hover{
+    transform: translate(-10px);
+  }
+  button::after{
+    content: "←";
+    display: none;
+    transition: .2s ease-in-out;
+  }
+  button:hover::after{
+    display: inline-block;
+  }
+</style>
+
+  <div class="text-xs fixed sm:text-base bottom-4 right-4 text-right z-10">
+    <button
+      class="bg-0 text-center py-2 px-4 mx-2"
+      style="
+        font-size: 1rem;
+        border-radius: 25px;
+        transition: .2s linear;
+      "
+    >
+      {#each items as { name, url }}
+        <a href={url} class="p-1 text-black" target="_blank" rel="noreferrer">{name}</a>
+      {/each}
+    </button>
+  </div>
+
